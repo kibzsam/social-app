58ff76b7431892cb30110bd49ac849c37eb72867
Sam Gichu <samgichu@Sams-MacBook-Pro.local>
2021-08-13 18:10:23 +0300

    Completed creating basic backend requirements

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-post-header/>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->count())
+          <x-post-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No Post yet.Come back another time</p>
+        @endif
+        </div>
+    </main>
+</x-layout>
